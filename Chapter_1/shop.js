52137053881bf357b8198608fd44d55b5f112beb
Matthew Nelson <mnelson104@gmail.com>
2025-01-29 19:46:12 -0500

    Updating files based on Chapter 4 stuff

--- Chapter_1/shop.js
+++ Chapter_1/shop.js
@@ -1,2 +1,14 @@
-// For now, display a quick message in the console.
+// Displaya quick message in the console.
 console.log("ClydeBank Coffee Shop is now open!")
+
+//Our inventory
+let inventory = ["Regular Coffee", "Espresso", "Cappuccino", "Latte"]
+let inventoryPrices = [3.00, 3.50, 4.00, 4.25]
+
+//Obtain reference to the menu list by ID
+let menuList = document.getElementById("coffee-menu")
+
+//Loop through the inventory and display each item in the menu list 
+for (let i=0; i < inventory.length;i++) {
+    menuList.innerHTML += "<li>" + inventory[i] + " - $" + inventoryPrices[i].toFixed(2) + "</li>"
+}
