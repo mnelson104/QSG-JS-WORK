e11b5c31e986cd8681c28cfba3bce6056bbad6df
mnelson104 <35145813+mnelson104@users.noreply.github.com>
2025-02-05 11:51:40 -0500

    Updated with Chapter 4 function change

--- Chapter_1/shop.js
+++ Chapter_1/shop.js
@@ -9,6 +9,9 @@ let inventoryPrices = [3.00, 3.50, 4.00, 4.25]
 let menuList = document.getElementById("coffee-menu")
 
 //Loop through the inventory and display each item in the menu list 
-for (let i=0; i < inventory.length;i++) {
-    menuList.innerHTML += "<li>" + inventory[i] + " - $" + inventoryPrices[i].toFixed(2) + "</li>"
+function populateMenu(container) {
+    for (let i=0; i < inventory.length;i++) {
+    container.innerHTML += "<li>" + inventory[i] + " - $" + inventoryPrices[i].toFixed(2) + "</li>"
+    }
 }
+populateMenu(menuList)
