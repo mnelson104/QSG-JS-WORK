e39e9b829e8ba3c347ada86e7eedbc1afb76908a
mnelson104 <35145813+mnelson104@users.noreply.github.com>
2025-02-10 23:14:27 -0500

    Updating with function from Ch 6

--- Chapter_1/shop.js
+++ Chapter_1/shop.js
@@ -1,17 +1,22 @@
 // Displaya quick message in the console.
 console.log("ClydeBank Coffee Shop is now open!")
 
-//Our inventory
-let inventory = ["Regular Coffee", "Espresso", "Cappuccino", "Latte"]
-let inventoryPrices = [3.00, 3.50, 4.00, 4.25]
-
-//Obtain reference to the menu list by ID
-let menuList = document.getElementById("coffee-menu")
-
-//Loop through the inventory and display each item in the menu list 
-function populateMenu(container) {
-    for (let i=0; i < inventory.length;i++) {
-    container.innerHTML += "<li>" + inventory[i] + " - $" + inventoryPrices[i].toFixed(2) + "</li>"
+//Th einventory object
+let menu = {
+    inventory: {
+        "Regular Coffee": 3.00,"Espresso": 3.50, "Cappuccino": 4.0,"Latte": 4.25
+    },
+    populate: function(container) {
+        for (let item in this.inventory)
+        {
+            let price = this.inventory[item]
+            container.innerHTML += "<li>" + item + " - $" + price.toFixed(2) + "</li>"
+        }
     }
 }
-populateMenu(menuList)
+
+//Obtain reference in the menu list by ID
+let menuList = document.getElementById("coffee-menu")
+
+//Populate the menu
+menu.populate(menuList)
